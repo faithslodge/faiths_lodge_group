fef65ea30a43d159c40a3282867446f287ecaea3
Tucker Melssen <tuckerjmelssen@gmail.com>
2024-03-04 14:27:18 -0600

    MapView page setup with MUI template. Removed a lot of bloat, organized components, added appropriate props with dummydata

---
+++ src/components/Map/OrganizationCard.jsx
@@ -0,0 +1,99 @@
+import * as React from 'react';
+import AspectRatio from '@mui/joy/AspectRatio';
+import Card from '@mui/joy/Card';
+import CardContent from '@mui/joy/CardContent';
+import CardOverflow from '@mui/joy/CardOverflow';
+import Link from '@mui/joy/Link';
+import Stack from '@mui/joy/Stack';
+import Typography from '@mui/joy/Typography';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import PhoneIcon from '@mui/icons-material/Phone';
+
+
+export default function RentalCard({ name, mission, image, cityState, phone }) {
+  return (
+    <Card
+      variant="outlined"
+      orientation="horizontal"
+      sx={{
+        bgcolor: 'neutral.softBg',
+        display: 'flex',
+        flexDirection: { xs: 'column', sm: 'row' },
+        '&:hover': {
+          boxShadow: 'lg',
+          borderColor: 'var(--joy-palette-neutral-outlinedDisabledBorder)',
+        },
+      }}
+    >
+
+      <CardOverflow
+        sx={{
+          mr: { xs: 'var(--CardOverflow-offset)', sm: 0 },
+          mb: { xs: 0, sm: 'var(--CardOverflow-offset)' },
+          '--AspectRatio-radius': {
+            xs: 'calc(var(--CardOverflow-radius) - var(--variant-borderWidth, 0px)) calc(var(--CardOverflow-radius) - var(--variant-borderWidth, 0px)) 0 0',
+            sm: 'calc(var(--CardOverflow-radius) - var(--variant-borderWidth, 0px)) 0 0 calc(var(--CardOverflow-radius) - var(--variant-borderWidth, 0px))',
+          },
+        }}
+      >
+        <AspectRatio
+          ratio="1"
+          flex
+          sx={{
+            minWidth: { sm: 120, md: 160 },
+            '--AspectRatio-maxHeight': { xs: '160px', sm: '9999px' },
+          }}
+        >
+          <img alt="" src={image} />
+        </AspectRatio>
+      </CardOverflow>
+
+
+
+      <CardContent>
+        <Stack
+          spacing={1}
+          direction="row"
+          justifyContent="space-between"
+          alignItems="flex-start"
+        >
+          <div>
+            <Typography level="title-md">
+              <Link
+                overlay
+                underline="none"
+                href="#interactive-card"
+                sx={{ color: 'text.primary' }}
+              >
+                {name}
+              </Link>
+            </Typography>
+          </div>
+        </Stack>
+        <Stack
+          spacing="0.25rem 1rem"
+          direction="row"
+          useFlexGap
+          flexWrap="wrap"
+          sx={{ my: 0.25 }}
+        >
+          <Typography level="body-xs" startDecorator={<LocationOnIcon />}>
+            {cityState}
+          </Typography>
+
+          <Typography level="body-xs" startDecorator={<PhoneIcon />}>
+            {phone}
+          </Typography>
+        </Stack>
+        <Stack direction="row">
+          <Typography
+            level="title-sm"
+            sx={{ display: 'flex', gap: 1 }}
+          >
+            {mission}
+          </Typography>
+        </Stack>
+      </CardContent>
+    </Card>
+  );
+}
--- src/pages/MapView/MapView.jsx
+++ src/pages/MapView/MapView.jsx
@@ -1,8 +1,72 @@
-const MapView = () => {
-    return (
-        <>
-        </>
-    )
+import * as React from 'react';
+import { CssVarsProvider } from '@mui/joy/styles';
+import CssBaseline from '@mui/joy/CssBaseline';
+import Box from '@mui/joy/Box';
+import Stack from '@mui/joy/Stack';
+
+import OrganizationCard from '../../components/Map/OrganizationCard';
+import Search from '../../components/Map/Search';
+import Filters from '../../components/Map/Filters';
+
+function MapView() {
+  return (
+    <CssVarsProvider disableTransitionOnChange>
+      {/* Main Container */}
+      <Box
+        component="main"
+        sx={{
+          height: '100vh', // 55px is the height of the NavBar
+          display: 'grid',
+          gridTemplateColumns: { xs: 'auto', md: '40% 60%' },
+          gridTemplateRows: 'auto 1fr auto',
+        }}
+      >
+
+        {/* Search Stack */}
+        <Stack
+          sx={{
+            backgroundColor: 'background.surface',
+            px: { xs: 2, md: 4 },
+            py: 2,
+            borderBottom: '1px solid',
+            borderColor: 'divider',
+          }}
+        >
+          <Search />
+        </Stack>
+
+
+        {/* !--- insert map into this Box, replace background img ---! */}
+        <Box
+          sx={{
+            gridRow: 'span 3',
+            display: { xs: 'none', md: 'flex' },
+            backgroundColor: 'background.level1',
+            backgroundSize: 'cover',
+            backgroundImage:
+              'url("https://images.unsplash.com/photo-1569336415962-a4bd9f69cd83?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3731&q=80")',
+          }}
+        />
+
+        {/* Organization Cards */}
+        <Stack spacing={2} sx={{ px: { xs: 2, md: 4 }, pt: 2, minHeight: 0 }}>
+          <Filters />
+          <Stack spacing={2} sx={{ overflow: 'auto' }}>
+
+            {/* Map DB info as props */}
+            <OrganizationCard
+              name="Faiths Lodge"
+              cityState="Minneapolis, MN"
+              phone="(123) 456-7890"
+              mission="Lorem ipsum la zell uffda sieta homen latilda"
+              image="https://images.unsplash.com/photo-1568605114967-8130f3a36994?auto=format&fit=crop&w=400"
+            />
+
+          </Stack>
+        </Stack>
+      </Box>
+    </CssVarsProvider>
+  );
 }
 
 export default MapView;
