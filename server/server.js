c6b937f94cdd0f8c033783feab35c9a1a500b2b3
Ryan Olson <137962315+RyanOlson86@users.noreply.github.com>
2024-03-04 09:52:33 -0600

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT || 5001;
+
+// Middleware Includes
+const sessionMiddleware = require('./modules/session-middleware');
+const passport = require('./strategies/user.strategy');
+
+// Route Includes
+const userRouter = require('./routes/user.router');
+
+// Express Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('build'));
+
+// Passport Session Configuration
+app.use(sessionMiddleware);
+
+// Start Passport Sessions
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Routes
+app.use('/api/user', userRouter);
+
+// Listen Server & Port
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
