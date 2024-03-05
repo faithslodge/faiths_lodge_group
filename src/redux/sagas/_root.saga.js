c5854dcce1ca1ebda599f52f31ce3cc04173ec3b
Tucker Melssen <tuckerjmelssen@gmail.com>
2024-03-05 10:27:11 -0600

    initial setup of org saga/reducer. proceed to testing

--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -2,7 +2,7 @@ import { all } from 'redux-saga/effects';
 import loginSaga from './login.saga';
 import registrationSaga from './registration.saga';
 import userSaga from './user.saga';
-import organizationSaga from './organization.saga';
+import organizationsSaga from './organizations.saga';
 import optionsSaga from './options.saga';
 
 // rootSaga is the primary saga.
@@ -17,7 +17,7 @@ export default function* rootSaga() {
     loginSaga(), // login saga is now registered
     registrationSaga(),
     userSaga(),
-    organizationSaga(),
+    organizationsSaga(),
     optionsSaga(),
   ]);
 }
