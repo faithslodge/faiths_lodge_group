b09bd1fd85c02308d6c302046dacbe9652a3f960
Ryan Olson <ryan.olson86@yahoo.com>
2024-03-05 10:43:00 -0600

    Updated styling of list and used store to populate list

--- src/components/OptionListItem/OptionListItem.jsx
+++ src/components/OptionListItem/OptionListItem.jsx
@@ -2,7 +2,7 @@ import { Button, ListItem, ListItemText } from "@mui/material"
 
 const OptionListItem = ({text}) => {
     return (
-        <ListItem  sx={{width: 250}}>
+        <ListItem  sx={{width: "auto"}}>
             <ListItemText primary={text} />
             <Button>📝 Edit</Button>
         </ListItem>
--- src/components/OptionsList/OptionsList.jsx
+++ src/components/OptionsList/OptionsList.jsx
@@ -1,19 +1,19 @@
 import { Box, Button, List, TextField } from "@mui/material";
 import OptionListItem from "../../components/OptionListItem/OptionListItem";
 
-const OptionsList = ({text}) => {
+const OptionsList = ({text, options}) => {
   return (
-    <Box sx={{ width: "fit-content", display: "flex", flexDirection:"column", justifyContent: "center", p:5 }}>
+    <Box sx={{ width: "fit-content", display: "flex", flexDirection:"column", justifyContent: "center", alignItems:"center", p:5 }}>
       <Box sx={{display: "flex", p: 1}}>
         <TextField size="small" label={text}/>
         <Button variant="contained">＋</Button>
       </Box>
 
       <Box sx={{border: "1px solid black", borderRadius: 3}}>
-        <List>
-          <OptionListItem text="Item 1" />
-          <OptionListItem text="Item 2" />
-          <OptionListItem text="Item 3" />
+        <List >
+          {options?.map(option => (
+            <OptionListItem text={option?.name} key={option?.id} />
+          ))}
         </List>
       </Box>
     </Box>
