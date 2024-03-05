02779a09ec62e7cc13a238455e16e24f3a5690aa
Ryan Olson <ryan.olson86@yahoo.com>
2024-03-05 14:26:23 -0600

    Added conditional rendering and edit buttons

--- src/components/OptionListItem/OptionListItem.jsx
+++ src/components/OptionListItem/OptionListItem.jsx
@@ -1,13 +1,45 @@
-import { Button, ListItem, ListItemText } from "@mui/material";
-import EditIcon from '@mui/icons-material/Edit';
+import { Button, ListItem, ListItemText, TextField } from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import SaveIcon from "@mui/icons-material/Save";
+import CancelIcon from "@mui/icons-material/Cancel";
+import { useState } from "react";
 
-const OptionListItem = ({text}) => {
-    return (
-        <ListItem  sx={{width: "auto"}}>
-            <ListItemText primary={text} />
-            <Button><EditIcon /></Button>
-        </ListItem>
-    )
-}
+const OptionListItem = ({ text }) => {
+  const [editView, setEditView] = useState(false);
+  const [newInput, setInput] = useState(text);
+
+  const handleEdit = () => {
+    editView ? setEditView(false) : setEditView(true);
+    setInput(text);
+  };
+
+  const handleSave = () => {
+    editView ? setEditView(false) : setEditView(true);
+    console.log(newInput);
+  };
+
+  return (
+    <ListItem sx={{ width: "auto" }}>
+      {editView ? (
+        <>
+          <TextField size="small" value={newInput} onChange={(event) => setInput(event.target.value)} />
+          <Button color="success" onClick={handleSave}>
+            <SaveIcon />
+          </Button>
+          <Button onClick={handleEdit} color="error">
+            <CancelIcon />
+          </Button>
+        </>
+      ) : (
+        <>
+          <ListItemText primary={text} />
+          <Button onClick={handleEdit}>
+            <EditIcon />
+          </Button>
+        </>
+      )}
+    </ListItem>
+  );
+};
 
 export default OptionListItem;
