b128d467dabe7995ff3b7e92cdc232ee28d217df
Dustin Hagstrom <dustnhgstrm@gmail.com>
2024-03-04 23:49:08 -0600

    created an organizationService file to handle making an organization with associated relations in the database, commented steps required to complete this. wrote a post route in the organization.router.js to reference the organizationService file when this route is hit. wrote comments in the addOrg.jsx file to define the body object that the server is expecting when an new organization is created.

--- src/pages/AddOrg/AddOrg.jsx
+++ src/pages/AddOrg/AddOrg.jsx
@@ -1,5 +1,66 @@
 
 const AddOrg = () => {
+    /*
+    dispatch({
+        type: 'CREATE_ORG',
+        payload: {
+            organizationDetails: {
+                org: {
+                    name,
+                    serviceExplanation,
+                    // logo,        <<<< this will implement later
+                    mission,
+                    notes,
+                    url,
+                    phone,
+                    email,
+                    forProfit,
+                    faithBased,
+                    hasRetreatCenter,
+                    linkedInUrl,
+                    facebookUrl,
+                    instagramUrl
+                },
+                address: {
+                    addressLineOne,
+                    addressLineTwo,
+                    city,
+                    state,
+                    stateAbbreviation,
+                    zipCode 
+                },
+                lossTypes: [
+                    lossType1,
+                    lossType2,
+                    ...
+                ],
+                serviceTypes: [
+                    serviceType1,
+                    serviceType2,
+                    ...
+                ],
+                contacts: [
+                    contact1,
+                    contact2,
+                    ...
+                ]
+            }
+        }
+    })
+
+<-------- BELOW IS FORMAT FOR A ORG CONTACT IN DISPATCH --------->
+
+        contact1 = {
+            firstName,
+            lastName,
+            phone,
+            email,
+            title
+        }
+
+<-------- ABOVE IS FORMAT FOR A ORG CONTACT IN DISPATCH --------->
+    */
+    
     return (
         <>
         </>
