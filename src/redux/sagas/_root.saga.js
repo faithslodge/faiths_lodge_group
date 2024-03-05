428f2f860d3cfe6e694924e4a6ce56407d5d9b1e
Ryan Olson <ryan.olson86@yahoo.com>
2024-03-05 09:26:14 -0600

    fixed missing comma

--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -17,7 +17,7 @@ export default function* rootSaga() {
     loginSaga(), // login saga is now registered
     registrationSaga(),
     userSaga(),
-    organizationSaga()
+    organizationSaga(),
     optionsSaga(),
   ]);
 }
