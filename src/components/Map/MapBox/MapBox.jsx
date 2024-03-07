16259e6b7a804a87a3ed46b70feffc0bfc5e26b5
Ryan Olson <ryan.olson86@yahoo.com>
2024-03-07 15:43:31 -0600

    Added map to MapView

--- src/components/Map/MapBox/MapBox.jsx
+++ src/components/Map/MapBox/MapBox.jsx
@@ -1,23 +1,32 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
-const MapBox = () => {
+const MapBox = ({ orgList }) => {
   // Set the initial position and zoom level of the map
   const position = ["44.985", "-93.25"]; // Coordinates for London
-  const zoom = 13;
+  const zoom = 4;
+
+  console.log(orgList);
 
   return (
-    <MapContainer center={position} zoom={zoom} style={{ height: '800px', width: '800px' }}>
+    <MapContainer center={position} zoom={zoom} style={{ height: "100%", width: "100%" }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={position}>
+      {/* <Marker position={position}>
         <Popup>
           Organization Name <br /> Link to Org Details
         </Popup>
-      </Marker>
+      </Marker> */}
+      {orgList.map((org) => (
+        <Marker key={org.id} position={[org?.latitude, org?.longitude]}>
+          <Popup>
+            {org.name} <br /> Link to Org Details
+          </Popup>
+        </Marker>
+      ))}
     </MapContainer>
   );
 };
