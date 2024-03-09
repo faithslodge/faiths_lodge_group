d22525a0c0468f36985cfe4d32f0943f7c2cce14
Dustin Hagstrom <dustnhgstrm@gmail.com>
2024-03-08 22:47:27 -0600

    wrote router for CRUD on organization_contact table in database, updated the Postman route json file. updated server file to reference new routes, added a comment to the organizationPostService file.

--- server/server.js
+++ server/server.js
@@ -1,21 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 const PORT = process.env.PORT || 5001;
 
 // Middleware Includes
-const sessionMiddleware = require('./modules/session-middleware');
-const passport = require('./strategies/user.strategy');
+const sessionMiddleware = require("./modules/session-middleware");
+const passport = require("./strategies/user.strategy");
 
 // Route Includes
-const userRouter = require('./routes/user.router');
-const organizationRouter = require('./routes/organization.router');
-const optionRouter = require('./routes/option.router');
+const userRouter = require("./routes/user.router");
+const organizationWithDetailsRouter = require("./routes/organizationWithDetails.router");
+const contactRouter = require("./routes/contact.router");
+const optionRouter = require("./routes/option.router"); // services types && loss types
 
 // Express Middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static('build'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("build"));
 
 // Passport Session Configuration
 app.use(sessionMiddleware);
@@ -25,11 +26,12 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Routes
-app.use('/api/user', userRouter);
-app.use('/api/organization', organizationRouter);
-app.use('/api/option', optionRouter);
+app.use("/api/user", userRouter);
+app.use("/api/organization", organizationWithDetailsRouter);
+app.use("/api/option", optionRouter);
+app.use("/api/contact", contactRouter);
 
 // Listen Server & Port
 app.listen(PORT, () => {
-  console.log(`Listening on port: ${PORT}`);
+    console.log(`Listening on port: ${PORT}`);
 });
