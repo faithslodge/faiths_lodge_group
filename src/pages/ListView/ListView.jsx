fcefbaac0aa4ccfca7cc6d8bf18a9f9d232fb80c
Abdulahi Kabare <abdullahikabare@gmail.com>
2024-03-13 06:59:08 -0500

    list view updated

--- src/pages/ListView/ListView.jsx
+++ src/pages/ListView/ListView.jsx
@@ -1,8 +1,51 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import { DataGrid } from "@mui/x-data-grid";
+import { useSelector } from "react-redux";
+
+const columns = [
+  {
+    field: "verified_by",
+    headerName: "Verify",
+  },
+  {
+    field: "name",
+    headerName: "Name",
+  },
+
+  {
+    field: "state",
+    headerName: "State",
+  },
+
+  {
+    field: "phone",
+    headerName: "Phone",
+  },
+];
+
+//agg_loss_type
+
 const ListView = () => {
-    return (
-        <>
-        </>
-    )
-}
+  const organizations = useSelector((store) => store.organizations);
+  console.log(organizations);
+  return (
+    <Box sx={{ height: 400, width: "100%" }}>
+      <DataGrid
+        rows={organizations}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 5,
+            },
+          },
+        }}
+        pageSizeOptions={[5]}
+        disableRowSelectionOnClick
+      />
+    </Box>
+  );
+};
 
 export default ListView;
