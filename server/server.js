fd6129654c98828a7412bae6b48330849ca1f89a
Dustin Hagstrom <dustnhgstrm@gmail.com>
2024-03-16 21:50:21 -0500

    changed the uploaded logo method from 'memory storage to database' to 'disk storage to database'. Added static file hosting of the public folder using express. made a 'logos' and 'tmp' folder. Added functionality to temporarily store uploaded images in the 'tmp' folder, then converted the image type to '.webp', stored the converted image in 'logos' and deleted the file that was uploaded to the 'tmp' folder.

--- server/server.js
+++ server/server.js
@@ -19,6 +19,7 @@ const logoRouter = require("./routes/logo.router");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("build"));
+app.use(express.static("public"));
 
 // Passport Session Configuration
 app.use(sessionMiddleware);
